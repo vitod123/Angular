222e9c05f4a088f2a9279e6bf3908c8a4a856272
Vitalii <vitalijmarchuk2508@gmail.com>
2023-04-13 11:17:58 +0200

    new_style

---
+++ src/app/to-do-list.ts
@@ -0,0 +1,5 @@
+export interface IT0_Do_list{
+    name: string;
+    date: string;
+    isCheck: boolean;
+}
--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IT0_Do_list } from '../to-do-list';
 
 @Component({
   selector: 'app-to-do-list',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./to-do-list.component.css']
 })
 export class ToDoListComponent {
-  
+  todolists: IT0_Do_list[];
+
+  constructor()
+  {
+    this.todolists = [
+      {name: "Hit the gym", date: "01.04.2023", isCheck: false},
+      {name: "Pay bills", date: "", isCheck: true},
+      {name: "Meet John", date: "", isCheck: true},
+      {name: "Buy eggs", date: "29.03.2023", isCheck: false},
+      {name: "Read a book", date: "", isCheck: true},
+      {name: "Organize office", date: "", isCheck: true},
+      {name: "Eat dinner", date: "", isCheck: true},
+      {name: "Buy apples", date: "05.03.2023", isCheck: false},
+      {name: "Meet George", date: "", isCheck: true},
+      {name: "Feed the cat", date: "", isCheck: true},
+      {name: "Wrile a letter", date: "", isCheck: true},
+      {name: "Run 1 km", date: "15.01.2022", isCheck: false}
+    ]
+  }
 }
